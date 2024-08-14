24773d0d5728e12bc3873689c7f04468c8be4882
Mmakgosana <makgakammakgosana@gmail.com>
2024-08-14 13:53:51 +0200

    I have made the trolley icon to not be hidden in small screens

--- my-vue-app/src/components/Header.vue
+++ my-vue-app/src/components/Header.vue
@@ -50,25 +50,27 @@
               </router-link>
             </li>
             <li class="hidden lg:block md:block relative">
-              <div class="t-0 absolute left-3 -top-4">
-                <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
-                  2
-                </p>
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="file:h-6 w-6 stroke-white cursor-pointer"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
+              <router-link to="/cart">
+                <div class="t-0 absolute left-3 -top-4">
+                  <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
+                    2
+                  </p>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="file:h-6 w-6 stroke-white cursor-pointer"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                  />
+                </svg>
+              </router-link>
             </li>
             <li>
               <router-link
@@ -102,7 +104,6 @@
 import { ref, onMounted, watch, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
-
 const isNavbarHidden = ref(true);
 const isLoggedIn = ref(!!localStorage.getItem('userToken'));
 const router = useRouter();
@@ -111,23 +112,12 @@ const toggleNavbar = () => {
   isNavbarHidden.value = !isNavbarHidden.value;
 };
 
-const handleLogin = async () => {
-  // Perform login logic here
-  // If login is successful:
-  localStorage.setItem('userToken', 'your_token_here');
-  isLoggedIn.value = true;
-  router.push('/'); // Redirect after login
-  // Force a reactivity update
-  isLoggedIn.value = !!localStorage.getItem('userToken');
-};
-
 const handleLogout = () => {
   localStorage.removeItem('userToken');
   isLoggedIn.value = false;
   router.push('/login');
 };
 
-// Watch for changes in localStorage and update isLoggedIn state
 watch(() => localStorage.getItem('userToken'), (newVal) => {
   isLoggedIn.value = !!newVal;
 });
