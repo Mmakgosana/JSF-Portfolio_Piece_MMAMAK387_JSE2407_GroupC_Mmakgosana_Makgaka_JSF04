c4d86520d7af18176193b1bfaf150f2b58c19b32
Mmakgosana <makgakammakgosana@gmail.com>
2024-08-15 09:59:17 +0200

    Updated tailwind.config.js to add light and dark mode color schemes and changed darkMode strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
-// tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  darkMode: 'class', // Enable dark mode using the 'class' strategy
   theme: {
     extend: {
       colors: {
-        customBg: '#F1E5D1',
-
+        light: {
+          background: '#ffffff',
+          text: '#1a202c',
+          primary: '#4a5568',
+          secondary: '#2d3748',
+          accent: '#38b2ac',
+        },
+        dark: {
+          background: '#1a202c',
+          text: '#f7fafc',
+          primary: '#cbd5e0',
+          secondary: '#a0aec0',
+          accent: '#63b3ed',
+        },
       },
     },
   },
   plugins: [],
-  darkMode: 'class',
 }
