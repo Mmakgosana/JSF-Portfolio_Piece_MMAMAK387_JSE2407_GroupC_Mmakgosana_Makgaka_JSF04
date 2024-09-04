c7cf4aa559321b0c8d94cc1f8352b1a371805995
Mmakgosana <makgakammakgosana@gmail.com>
2024-09-04 11:40:19 +0200

    "Removed cart link and icon from Header component"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -87,33 +87,8 @@
                 </svg>
               </router-link>
             </li>
-            <li class="relative">
-  <router-link
-    to="/cart"
-    class="flex items-center text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-  >
-    <!-- Cart Icon -->
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="h-6 w-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-      />
-    </svg>
+            
 
-    <!-- Cart Item Count Badge -->
-    <div v-if="totalItems > 0" class="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
-      {{ totalItems }}
-    </div>
-  </router-link>
-</li>
 
 
             <li v-if="!isLoggedIn">
@@ -144,7 +119,6 @@ import { ref, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useCart } from '../CartStore';
 import { useWishlistStore } from '../WishlistStore';
-
 
 const isNavbarHidden = ref(true);
 const isLoggedIn = ref(!!localStorage.getItem('userToken'));
