a725fc8d7bd61340b05705d2313d4d7c45d41454
Mmakgosana <makgakammakgosana@gmail.com>
2024-09-04 11:52:54 +0200

    I have added a comparison icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -64,7 +64,30 @@
                 </svg>
               </router-link>
             </li>
-            <li class="hidden lg:block md:block relative">
+            <li class="relative">
+              <router-link to="/comparison">
+                <div class="t-0 absolute left-3 -top-4">
+                  <p class="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
+                    {{ comparisonItemCount }}
+                  </p>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="file:h-6 w-6 stroke-white cursor-pointer"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm0 2v12h16V6H4zm7 5h2v2h-2v-2z"
+                  />
+                </svg>
+              </router-link>
+            </li>
+            <li class="relative">
               <router-link to="/cart">
                 <div class="t-0 absolute left-3 -top-4">
                   <p class="flex h-6 w-6 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
@@ -87,9 +110,6 @@
                 </svg>
               </router-link>
             </li>
-            
-
-
 
             <li v-if="!isLoggedIn">
               <router-link
@@ -119,16 +139,19 @@ import { ref, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useCart } from '../CartStore';
 import { useWishlistStore } from '../WishlistStore';
+import { useComparisonStore } from '../ComparisonStore'; // Import your comparison store
 
 const isNavbarHidden = ref(true);
 const isLoggedIn = ref(!!localStorage.getItem('userToken'));
 const router = useRouter();
 const cartStore = useCart();
 const wishlistStore = useWishlistStore();
+const comparisonStore = useComparisonStore(); // Initialize comparison store
 
-// Computed properties for wishlist and cart item counts
+// Computed properties for wishlist, cart, and comparison item counts
 const wishlistItemCount = computed(() => wishlistStore.items.length);
 const totalItems = computed(() => cartStore.totalItems);
+const comparisonItemCount = computed(() => comparisonStore.items.length); // Add this for comparison
 
 const toggleNavbar = () => {
   isNavbarHidden.value = !isNavbarHidden.value;
